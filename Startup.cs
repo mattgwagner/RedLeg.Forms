b40704451e699c49f0cb7799d52ced3e568bfbd8
Matt Wagner <mattgwagner@gmail.com>
2020-04-26 20:49:49 -0400

    Fix default route

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -17,6 +13,10 @@ namespace RedLeg.Forms
         public void ConfigureServices(IServiceCollection services)
         {
             // Add Swagger Gen
+
+            services
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Latest);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,7 +27,14 @@ namespace RedLeg.Forms
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
 
             // Add Swagger UI
         }
