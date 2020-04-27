f9c23b0fce1b1705c5697cdb895b35c538e9e478
Matt Wagner <mattgwagner@gmail.com>
2020-04-26 22:11:22 -0400

    Fix weird class naming

--- Counseling.cs
+++ Counseling.cs
@@ -2,29 +2,26 @@
 
 namespace RedLeg.Forms
 {
-    public partial class FormController
+    public class Counseling
     {
-        public class Counseling
-        {
-            public String Name { get; set; }
+        public String Name { get; set; }
 
-            public Rank Rank { get; set; }
+        public Rank Rank { get; set; }
 
-            public DateTime Date { get; set; } = DateTime.Today;
+        public DateTime Date { get; set; } = DateTime.Today;
 
-            public String Organization { get; set; }
+        public String Organization { get; set; }
 
-            public String Counselor { get; set; }
+        public String Counselor { get; set; }
 
-            public String Purpose { get; set; }
+        public String Purpose { get; set; }
 
-            public String KeyPointsOfDiscussion { get; set; }
+        public String KeyPointsOfDiscussion { get; set; }
 
-            public String PlanOfAction { get; set; }
+        public String PlanOfAction { get; set; }
 
-            public String LeadersResponsibilities { get; set; }
+        public String LeadersResponsibilities { get; set; }
 
-            public String Assessment { get; set; }
-        }
+        public String Assessment { get; set; }
     }
 }
