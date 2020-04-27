d435800e94a85c05bf00d9dc30c21b9b61a318f8
Matt Wagner <mattgwagner@gmail.com>
2020-04-26 20:30:53 -0400

    Add base API boilerplate

---
+++ FormController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RedLeg.Forms
+{
+    public class FormController : Controller
+    {
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return Content("Hello world!", "text/plain");
+        }
+    }
+}
