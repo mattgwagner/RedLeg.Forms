3021b16e6ff0bb9e41a99c3729605eba2e499009
Matt Wagner <mattgwagner@gmail.com>
2020-04-30 13:30:53 -0400

    Add Seq logging for project
    Fixes #12

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Core;
 
 namespace RedLeg.Forms
 {
     public class Program
     {
+        public static LoggingLevelSwitch LogLevel { get; } = new LoggingLevelSwitch(Serilog.Events.LogEventLevel.Information);
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -17,6 +22,20 @@ namespace RedLeg.Forms
                     webBuilder
                         .UseSentry(dsn: "https://31730b790946460e8c5c786136c81231@o255975.ingest.sentry.io/5218373")
                         .UseStartup<Startup>();
+                })
+                .ConfigureLogging((context, builder) =>
+                {
+                    Log.Logger =
+                           new LoggerConfiguration()
+                           .Enrich.FromLogContext()
+                           .Enrich.WithProperty("Application", context.HostingEnvironment.ApplicationName)
+                           .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                           .Enrich.WithProperty("Version", $"{typeof(Startup).Assembly.GetName().Version}")
+                           .WriteTo.Seq(serverUrl: "https://logs.redleg.app", apiKey: context.Configuration.GetValue<string>("Seq:ApiKey"), compact: true, controlLevelSwitch: LogLevel)
+                           .MinimumLevel.ControlledBy(LogLevel)
+                           .CreateLogger();
+
+                    builder.AddSerilog();
                 });
     }
 }
