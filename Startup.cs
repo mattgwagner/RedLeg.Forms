edcf868844e0ec8b658b1f800eede11c9c28b0ec
Matt Wagner <Matt@Red-Leg-Dev.com>
2020-05-03 22:55:59 -0400

    Bump

--- Startup.cs
+++ Startup.cs
@@ -14,7 +14,6 @@ namespace RedLeg.Forms
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
             // Who needs the owner password?
 
             iTextSharp.text.pdf.PdfReader.AllowOpenWithFullPermissions = true;
